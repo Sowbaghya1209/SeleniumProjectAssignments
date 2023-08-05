4d9e0b61d2d2b75af8978d5a90349bdd6121da1e
Sowbaghya1209 <140327108+Sowbaghya1209@users.noreply.github.com>
2023-08-05 18:22:13 +0530

    Add files via upload

---
+++ Week3Day1/Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop obj=new Desktop();
+		obj.computerModel();
+		obj.desktopSize();
+
+	} 
+public void desktopSize()
+{
+	
+	System.out.println("Derived class:32 inches");
+}
+}
